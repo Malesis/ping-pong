9e7d60f2e2588a1598881399196084a08773613d
Malesis <143174761+Malesis@users.noreply.github.com>
2024-08-30 20:01:00 +1200

    egg_war0.2.py
    mechanics for players and EGS and more

--- egg_war.py
+++ egg_war.py
@@ -15,12 +15,18 @@ class GameSprite(sprite.Sprite):
 
 
 class Player(GameSprite):
-    def update(self):
+    def update_R(self):
         keys_pressed = key.get_pressed()
-        if keys_pressed[K_d] and self.rect.x < 645:
-            self.rect.x += self.speed
-        if keys_pressed[K_a] and self.rect.x > 0:
-            self.rect.x -= self.speed
+        if keys_pressed[K_s] and self.rect.y < 330:
+            self.rect.y += self.speed
+        if keys_pressed[K_w] and self.rect.y > 0:
+            self.rect.y -= self.speed
+    def update_L(self):
+        keys_pressed = key.get_pressed()
+        if keys_pressed[K_DOWN] and self.rect.y < 330:
+            self.rect.y += self.speed
+        if keys_pressed[K_UP] and self.rect.y > 0:
+            self.rect.y -= self.speed
         
 
 window = display.set_mode((700,500))
@@ -34,12 +40,17 @@ lose = font1.render('YOU LOSE!',True,(255,215,0))
 
 
 
-player1 = Player('1 player.png',0,200,100,170,5)
-player2 = Player('2 player.jpg',600,200,100,170,5)
+player1 = Player('1 player.png',50,200,100,170,5)
+player2 = Player('2 player.png',550,200,100,170,5)
+
+
+egg = GameSprite('egg.png',325,250,50,60,3)
 
 
 clock = time.Clock()
 FPS = 60
+speed_x = 3
+speed_y = 3
 game = True
 finish = False
 while game:
@@ -48,12 +59,26 @@ while game:
             game = False
     if finish != True:
         window.blit(background,(0,0))
+
+
+        egg.rect.x += speed_x
+        egg.rect.y += speed_y
+    
     
         
-        player1.update()
+        player1.update_R()
         player1.reset()
-        player2.update()
+        player2.update_L()
         player2.reset()
+        egg.update()
+        egg.reset()
+
+
+        if egg.rect.y <= 0 or egg.rect.y >= 440:
+            speed_y *= -1
+        if  sprite.collide_rect(player1,egg) or sprite.collide_rect(player2,egg):
+            speed_x *= -1
+        
 
     clock.tick(FPS)
     display.update()
